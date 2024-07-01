be80864d9ca99cce54489c2228488f88e53a9b58
ohyeseong <ooo5912519@naver.com>
2024-07-01 13:31:18 +0900

    jquery 시작

---
+++ script/index6.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(".btn1").click(function(){
+        $("body").animate({"background-color":"red"},600)
+    })// .btn1.click
+
+    $(".btn2").click(function(){
+        $("body").animate({"background-color":"yellow"},600)
+    })// .btn2.click
+
+    $(".btn3").click(function(){
+        $("body").animate({"background-color":"green"},600)
+    })// .btn3.click
+
+    $(".btn4").click(function(){
+        $("body").animate({"background-color":"blue"},600)
+    })// .btn4.click
+})// .jquery end
---
+++ script/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    $(".btn").click(function(){
+        // $(".img_box").css({"width":"100%"})
+        $(".img_box").animate({"width":"100%","opacity":"1"}, 500);
+    })// .btn.click
+})// jQuery end
